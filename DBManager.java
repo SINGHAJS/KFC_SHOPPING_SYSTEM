e86991c396090f3252151132429ba44ff1d8e05c
Ajit Singh <Ajit_Singh_1@outlook.com>
2021-09-20 16:57:33 +1200

    Adds new DBManager class

---
+++ DBManager.java
@@ -0,0 +1,53 @@
+package KFC_SHOPPING_SYSTEM;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Ajit Singh
+ * ID: 19070642
+ * 
+ */
+
+
+public class DBManager {
+    private static final String USER_NAME = "kfc"; //DB username
+    private static final String PASSWORD = "kfc"; //DB password
+    private static final String URL = "jdbc:derby://localhost:1527/KFC_DB; create=true";  //url of the DB host
+
+     Connection conn;
+    
+    public DBManager(){
+        establishConnection();
+    }
+    
+    public Connection getConnection() {
+        return this.conn;
+    }
+    
+    //Establish connection
+    public void establishConnection() {
+        if (this.conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+                System.out.println(URL + " CONNECTION SUCCESSFUL");
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+      public void closeConnections() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+    
+}
