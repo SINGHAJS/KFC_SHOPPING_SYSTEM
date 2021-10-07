0e7dc7a9507da111636572a971f45572f33604ba
Ajit Singh <Ajit_Singh_1@outlook.com>
2021-10-07 23:05:11 +1300

    Add comments to the class

--- CustomerRegistration.java
+++ CustomerRegistration.java
@@ -11,7 +11,21 @@ import javax.swing.JOptionPane;
  */
 public class CustomerRegistration {
 
+    /**
+     * @param aRegView
+     * @param aCustomerTable
+     * @param DBM
+     * @param URL
+     * @param aView
+     * @param homeView
+     *
+     * gets text from the swing components in the RegistrationView.java class
+     * and check if the text matches the requirements. If so, add the details to
+     * the database
+     */
     public static void aNewCustomer(RegistrationView aRegView, String aCustomerTable, DBManager DBM, String URL, LoginView aView, HomeView homeView) {
+        // define variables 
+        // set the variables values to the swing components from the RegistrationView.java class
         String nameField = aRegView.aNameField.getText();
         String passField = aRegView.aPassField.getText();
         String confirmPassField = aRegView.aConfirmPasswordField.getText();
@@ -30,8 +44,10 @@ public class CustomerRegistration {
                     DBM = new DBManager(URL);
                     Statement st = DBM.getConnection().createStatement();
                     st.execute(insertCustomerTableValues);
-                    
-                    shopNowMessage(aRegView, aView, homeView);                    
+
+                    /* uses the shopNowMessage function to allow the user to go 
+                    straight to the HomeView.java View upon successful registration */
+                    shopNowMessage(aRegView, aView, homeView);
                 } catch (SQLException ex) {
                     System.out.println("[ERROR:" + ex + "]");
                     JOptionPane.showMessageDialog(aRegView.aRegistrationFrame, "SQL EXCEPTION OCCURED", "ERROR", JOptionPane.ERROR_MESSAGE);
@@ -42,18 +58,29 @@ public class CustomerRegistration {
         }
     }
 
+    /**
+     *
+     * @param aRegView
+     * @param aView
+     * @param homeView
+     *
+     * stores the user input of "Yes", "No" or "X" into an variable name "input"
+     * uses input data to allow the log in to the home view from the
+     * RegistrationView.java class View or let the user to go back to the
+     * LoginView.java class View
+     */
     public static void shopNowMessage(RegistrationView aRegView, LoginView aView, HomeView homeView) {
         int input = JOptionPane.showConfirmDialog(aRegView.aRegistrationFrame, "THANK YOU FOR REGISTERING, FINGER LIKING DEALS ARE WAITING FOR YOU AT KFC!\nSHOP NOW?", "REGISTRATION SUCCESSFUL", JOptionPane.YES_NO_OPTION);
         switch (input) {
-            case 0:
+            case 0: // user inputs "Yes" as the response
                 aRegView.aRegistrationFrame.setVisible(false);
                 homeView.aHomeFrame.setVisible(true);
                 break;
-            case 1:
+            case 1: // user inputs "No" as the response
                 aRegView.aRegistrationFrame.setVisible(false);
                 aView.aLoginPageFrame.setVisible(true);
                 break;
-            case -1:
+            case -1: // user inputs "X" as the response
                 aRegView.aRegistrationFrame.setVisible(true);
                 break;
             default:
