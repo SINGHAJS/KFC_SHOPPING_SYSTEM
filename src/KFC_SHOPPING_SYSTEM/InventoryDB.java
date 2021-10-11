861027c23908bcb5cc554bd4572c235861643fd9
Rohit Singh <singhrohit7299@gmail.com>
2021-10-12 01:30:17 +1300

    added get items feature

--- src/KFC_SHOPPING_SYSTEM/InventoryDB.java
+++ src/KFC_SHOPPING_SYSTEM/InventoryDB.java
@@ -20,18 +20,19 @@ public class InventoryDB {
     private static final String URL = "jdbc:derby:KFC_Inventory;create=true";
     private static final String KFC_MENU = "./resources/KFC_MENU.txt";
     private static ArrayList<String> list = new ArrayList<>();
-    private static final String TableName = "INVENTORY";
+    private static final String tableName = "INVENTORY";
 
     public InventoryDB(){
         this.db = new DBManager(URL);
     }
-    
+
     public void createItemsTable(){
         try {
             
-            if (!db.checkTable(TableName)){
-                db.getStatement().executeUpdate("CREATE TABLE " + TableName + "(ID INT, "
-                        + "CATEGORY VARCHAR(50),ITEM_NAME VARCHAR(50), ITEM_PRICE FLOAT)");
+            if (!db.checkTable(tableName)){
+                db.getStatement().executeUpdate("CREATE TABLE " + tableName + "(ID INT, "
+                        + "CATEGORY VARCHAR(50),ITEM_NAME VARCHAR(50), ITEM_PRICE DOUBLE)");
+                fillTable();
             }
             db.getStatement().close();
         } catch (SQLException e) {
@@ -47,7 +48,7 @@ public class InventoryDB {
             for(String o: list){
                 String[] itemInfo = o.split(",");
                 Double price = Double.parseDouble(itemInfo[2]);
-                String sqlInsert = "INSERT INTO " + TableName + " VALUES ("+ count++ + ",'"+ itemInfo[0]+ "','" + itemInfo[1]+ "', "+price+")";
+                String sqlInsert = "INSERT INTO " + tableName + " VALUES ("+ count++ + ",'"+ itemInfo[0]+ "','" + itemInfo[1]+ "', "+price+")";
                 System.out.println(sqlInsert);
                db.getStatement().executeUpdate(sqlInsert);
             }
@@ -57,18 +58,33 @@ public class InventoryDB {
     }
     public Vector getCategory(){
         ResultSet rs = null;
-        Vector<String> v = null;
+        Vector<String> category = new Vector<String>();
         try{
-           // db.getStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            String sqlQuery = "SELECT CATEGORY";
-            //rs = statement.executeQuery(sqlQuery);
-            rs.beforeFirst();
+            String sqlQuery = "SELECT CATEGORY FROM " + tableName;
+            rs = db.getStatement().executeQuery(sqlQuery);
             while(rs.next()){
-                v.add(rs.getString("CATEGORY"));
+                category.add(rs.getString("CATEGORY"));
             }
         }catch(SQLException e){
-            System.out.println("get cat");
+             Logger.getLogger(InventoryDB.class.getName()).log(Level.SEVERE, null, e);
         }
-        return v;
+        return category;
     }
+    public Vector getItems(String categoryName){
+        ResultSet rs = null;
+        Vector<Products> items = new Vector<Products>();
+        try{
+            String sqlQuery = "Select ITEM_NAME, ITEM_PRICE from " + tableName + " where CATEGORY='" + categoryName + "'";
+            rs = db.getStatement().executeQuery(sqlQuery);
+            while(rs.next()){
+                String iname = rs.getString("ITEM_NAME");
+                double price = rs.getDouble("ITEM_PRICE");
+                items.add(new Products(categoryName,iname,price));
+            }
+        }catch(SQLException e){
+            Logger.getLogger(InventoryDB.class.getName()).log(Level.SEVERE, null, e);
+        }
+        return items;
+    }
+   
 }
