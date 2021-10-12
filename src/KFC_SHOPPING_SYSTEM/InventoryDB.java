3fd39e67f0ef64d22c2f262e58c32cfa719cb71b
Rohit Singh <singhrohit7299@gmail.com>
2021-10-12 14:26:44 +1300

    removed unused imports

--- src/KFC_SHOPPING_SYSTEM/InventoryDB.java
+++ src/KFC_SHOPPING_SYSTEM/InventoryDB.java
@@ -1,12 +1,8 @@
 package KFC_SHOPPING_SYSTEM;
 
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
