c8ad2fae4d97436b6cfa24fb45b29fdb0b09ba68
Rohit Singh <singhrohit7299@gmail.com>
2021-10-12 14:34:10 +1300

    Browse frame added

---
+++ src/KFC_SHOPPING_SYSTEM/BrowseData.java
@@ -0,0 +1,13 @@
+package KFC_SHOPPING_SYSTEM;
+
+import java.util.Vector;
+
+/**
+ *
+ * @author singh
+ */
+public class BrowseData {
+    boolean browseFlag = false;
+    Vector<String> categories = new Vector<String>();
+    Vector<Products> items = new Vector<Products>();
+}
